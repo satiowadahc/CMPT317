e5b55435a01558c4463d57137e87cd63691cb41a
Chad A. Woitas <woitas.chad@gmail.com>
2018-02-05 22:28:14 -0600

    Imported networkx and matplotlib, working copy

--- A1.py
+++ A1.py
@@ -1,5 +1,32 @@
-print("Helloworld")
+# Chad A. Woitas
+# CMPT 317 A1
+# Due Date February 9
+#
 
-#wtf
-#change3
-#change4
+# Plan
+# --------------------------------
+# startSpace
+# All Vehicles start at (0,0,0) y=3
+# Packages are randomly distributed
+# Delivery spots are random
+
+# state
+# vehicle location(s) = m
+# package location(s) = n
+# vehicleHasPackage
+
+# successor function must
+
+# ---------------------------------
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+city = nx.grid_graph(dim=[3, 4])
+
+# pos = nx.spring_layout(city, iterations=100)
+
+plt.subplot()
+nx.draw(city, font_size=8)
+
+plt.show()
