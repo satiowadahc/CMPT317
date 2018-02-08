f75a37e70da17105b7f081206e8c2f6e09045d75
Chad A. Woitas <woitas.chad@gmail.com>
2018-02-07 21:10:59 -0600

    Is Goal Created, Problem State Finalized

--- A1.py
+++ A1.py
@@ -22,13 +22,13 @@ import networkx as nx
 import random as rng
 import matplotlib.pyplot as plt
 
-# city = nx.grid_graph(dim=[10, 10])
 
-# plt.subplot()
-# nx.draw(city, font_size=1)
-
-# plt.show()
-
+# PARAMETERS ----------------------------------
+trucksNum = 1
+packsNum = 1
+# gridy = 1
+gridx = 10
+# PARAMETERS ----------------------------------
 
 class Package:
 
@@ -40,11 +40,16 @@ class Package:
 
 
 class Vehicle:
+    location = nx.Graph()
+    cargo = []
+    vid = 0
 
-    def __init__(self, g):
+    def __init__(self, g, vid):
+        global location, cargo
         self.location = g
         self.packageLimit = 1
         self.cargo = []
+        self.vid = vid
 
     def pickupPackage(self, package):
         if self.cargo.count() < self.packageLimit:
@@ -85,41 +90,76 @@ class StateQueue:
         return True
 
 
-# TODO: Implement
 class ProblemState:
     # Next do this
+    vehicles = []
+    packages = []
 
-    def __init__(self, vehicles, packages):
-        self.vehicles = vehicles
-        self.packages = packages
+    def __init__(self, newvehicles, newpacks):
+        self.vehicles = newvehicles
+        self.packages = newpacks
 
     def displayState(self):
         print("Trucks:")
         print("-------")
-        print(self.vehicles.location, self.vehicles.cargo)
+        for i in range(len(self.vehicles)):
+            current = self.vehicles
+            if len(current[i].cargo) > 0:
+                print(current[i].location, current[i].cargo)
+            else:
+                print(current[i].location)
+        print()
+
         print("Packages:")
         print("-------")
-        print(self.packages.location, self.packages.source, self.packages.destination)
+        for i in range(len(self.packages)):
+            current = self.packages
+            print(current[i].location,
+                  current[i].source,
+                  current[i].destination)
 
 
 class Problem:
+    trucks = []
+    packs = []
 
     def __init__(self):
-        global truck, p1
-        G = nx.grid_graph([10])
-
+        global trucks, packs
+        trucks = []
+        packs = []
+        # Graph Creation
+        G = nx.grid_graph([gridx])
         for i in range(G.number_of_nodes()):
             G.add_node(i, node=i)
 
-        truck = Vehicle(G.node[0])
-        p1 = Package(G.node[rng.randint(1, 9)], G.node[rng.randint(1, 9)], 1)
-        print("Truck Starting at", truck.location)
-        print("Package 1 Starting", p1.source, "Destination", p1.destination, "Current Location", p1.location)
+        # Trucks Creation
+        for i in range(trucksNum):
+            newTruck = Vehicle(G.node[1], i)
+            trucks.append(newTruck)
 
+        # Packages Creation
+        for i in range(packsNum):
+            x = rng.randint(1, G.number_of_nodes()-1)
+            y = rng.randint(1, G.number_of_nodes()-1)
+            while x == y:
+                y = rng.randint(1, G.number_of_nodes())
+            newPack = Package(G.node[x], G.node[y], 1)
+            packs.append(newPack)
 
 # packages at destination
     def isGoal(self):
-        return True
+        test = 0
+
+        for i in range(len(packs)):
+            if packs[i].location == packs[i].destination:
+                test += 1
+
+        print("Goal Completion:", test*100/len(packs), "%")
+
+        if test == len(packs):
+            return True
+        else:
+            return False
 
     def successors(self):
         return True
@@ -129,6 +169,8 @@ class Problem:
 
 problem = Problem()
 
-problemState = ProblemState(truck, p1)
+problemState = ProblemState(trucks, packs)
 
 problemState.displayState()
+
+problem.isGoal()
