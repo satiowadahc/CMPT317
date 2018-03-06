0742ce3d45bf4f52e578d1454035bab09a2d7df9
Brandon Bachynski <brandon.bachynski@gmail.com>
2018-03-05 19:03:30 -0600

    Fixing deliverPackage method

--- A2.py
+++ A2.py
@@ -29,3 +29,29 @@ class pawn:
     @staticmethod
     def display():
         return 'p'
+
+
+def minimax(start):
+    transpositionTable = dict()
+
+    def do_minimax(node):
+        s = node.str()
+        if s in transpositionTable:
+            return transpositionTable[s]
+        elif node.isTerminal():
+            u = node.utility()
+        else:
+            vs = [do_minimax(c) for c in node.successors()]
+            if node.isMaxNode():
+                u = max(vs)
+            elif node.isMinNode():
+                u = min(vs)
+            else:
+                print("Something went horribly wrong")
+                return None
+        transpositionTable[s] = u
+        return u
+
+    result = do_minimax(start)
+    # print(transpositionTable)
+    return result
