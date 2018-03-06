5f19a0e791cf5596697fd40d780be0d627df64b4
Chad A. Woitas <woitas.chad@gmail.com>
2018-03-06 10:03:24 -0600

    Pawn Movements Complete and tested, next adding dragon and queen

--- A2.py
+++ A2.py
@@ -45,20 +45,43 @@ class dragon:
 
 
 class pawn:
-    def __init__(self,x,y):
-        self.x
-        self.y
-    def forward(self):
-        if self.x!=5:
-            self.x-=1
+    x = 0
+    y = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def backward(self):
-        if self.x!=1:
-            self.x+=1
-
-    def left(self):
-        if self.y!=1:
-            self.y-=1
+    def nextAvailableMoves(self):
+        # 1 Free Movement
+        if 1 < self.x < 5 and 1 < self.y < 5:
+            return {(self.x, self.y - 1), (self.x-1, self.y), (self.x+1, self.y), (self.x, self.y+1)}
+        # 2 Can't Go back
+        elif self.x == 1 and 1 < self.y < 5:
+            return {(self.x, self.y - 1), (self.x + 1, self.y), (self.x, self.y + 1)}
+        # 3 Can't go left
+        elif 1 < self.x < 5 and self.y == 1:
+            return {(self.x - 1, self.y), (self.x + 1, self.y), (self.x, self.y + 1)}
+        # 4 Can't go back or left
+        elif self.x == 1 and self.y == 1:
+            return {(self.x + 1, self.y), (self.x, self.y + 1)}
+        # 5 Can't go forward
+        elif self.x == 5 and 1 < self.y < 5:
+            return {(self.x, self.y - 1), (self.x-1, self.y), (self.x, self.y + 1)}
+        # 6 Can't go right
+        elif 1 < self.x < 5 and self.y == 5:
+            return {(self.x, self.y - 1), (self.x-1, self.y), (self.x + 1, self.y)}
+        # 7 Can't go forward or right
+        elif self.x == 5 and self.y == 5:
+            return {(self.x, self.y - 1), (self.x-1, self.y)}
+        # 8 Can't go forward or Left
+        elif self.x == 5 and self.y == 1:
+            return {(self.x-1, self.y), (self.x, self.y + 1)}
+        # 9 Can't go back or right
+        elif self.x == 1 and self.y == 5:
+            return {(self.x, self.y - 1),(self.x + 1, self.y)}
+        # 10 probably could throw an error
+        else:
+            print("somethings fucky")
 
     @staticmethod
     def display():
@@ -73,3 +96,9 @@ class pawn:
 # class problem:
 #     def __init__(self):
 #         Board board = Board.__init__()
+
+
+p = pawn(1, 5)
+print(p.x, p.y)
+
+print(p.nextAvailableMoves())
