0eb857ddafbd8d9413dcd4a1b6e8a47504d5b35f
Chad A. Woitas <woitas.chad@gmail.com>
2018-03-06 12:28:22 -0600

    Added players to board, added update board and display board. All Tested

--- A2.py
+++ A2.py
@@ -3,25 +3,60 @@
 # Chad A. Woitas
 
 
-class Board:
+class board:
 
     def __init__(self):
         self.x = 5
         self.y = 5
 
-        self.Q = queen(1, 3)
+        self.board = [[0 for x in range(self.x)] for y in range(self.y)]
 
-        self.D1 = dragon(2, 2)
-        self.D2 = dragon(2, 3)
-        self.D3 = dragon(2, 4)
+        self.Q = queen(0, 2)
 
-        self.P1 = pawn(5, 1)
-        self.P2 = pawn(5, 2)
-        self.P3 = pawn(5, 3)
-        self.P4 = pawn(5, 4)
-        self.P5 = pawn(5, 5)
+        self.D1 = dragon(1, 1)
+        self.D2 = dragon(1, 2)
+        self.D3 = dragon(1, 3)
 
-    # def display(self):
+        self.P1 = pawn(4, 0)
+        self.P2 = pawn(4, 1)
+        self.P3 = pawn(4, 2)
+        self.P4 = pawn(4, 3)
+        self.P5 = pawn(4, 4)
+
+        self.updateBoard()
+
+    def updateBoard(self):
+        (m, n) = self.Q.getCurrentPosition()
+        self.board[m][n] = self.Q
+
+        (m, n) = self.D1.getCurrentPosition()
+        self.board[m][n] = self.D1
+        (m, n) = self.D2.getCurrentPosition()
+        self.board[m][n] = self.D2
+        (m, n) = self.D3.getCurrentPosition()
+        self.board[m][n] = self.D3
+
+        (m, n) = self.P1.getCurrentPosition()
+        self.board[m][n] = self.P1
+        (m, n) = self.P2.getCurrentPosition()
+        self.board[m][n] = self.P2
+        (m, n) = self.P3.getCurrentPosition()
+        self.board[m][n] = self.P3
+        (m, n) = self.P4.getCurrentPosition()
+        self.board[m][n] = self.P4
+        (m, n) = self.P5.getCurrentPosition()
+        self.board[m][n] = self.P5
+
+    def display(self):
+        for i in range(self.x):
+            for j in range(self.y):
+                if isinstance(self.board[i][j], queen) or \
+                   isinstance(self.board[i][j], dragon) or \
+                   isinstance(self.board[i][j], pawn):
+                    print(self.board[i][j].display(), end='')
+                else:
+                    print(self.board[i][j], end='')
+            print('')
 
 
 class queen:
@@ -72,6 +107,9 @@ class queen:
                     (self.x + 1, self.y - 1)}
         else:
             print("Somethings fucky")
+
+    def getCurrentPosition(self):
+        return self.x, self.y
 
     @staticmethod
     def display():
@@ -127,6 +165,9 @@ class dragon:
         else:
             print("Somethings fucky")
 
+    def getCurrentPosition(self):
+        return self.x, self.y
+
     @staticmethod
     def display():
         return 'd'
@@ -172,6 +213,9 @@ class pawn:
         else:
             print("somethings fucky")
 
+    def getCurrentPosition(self):
+        return self.x, self.y
+
     @staticmethod
     def display():
         return 'p'
@@ -187,7 +231,11 @@ class pawn:
 #         Board board = Board.__init__()
 
 
-p = pawn(1, 5)
-print(p.x, p.y)
+# Begin unit tests
+p = pawn(1, 2)
 
+print(p.getCurrentPosition())
 print(p.nextAvailableMoves())
+
+b = board()
+b.display()
