54620c99c98401cc7e9243348abbe2d031f1f420
Chad A. Woitas <woitas.chad@gmail.com>
2018-03-06 09:22:05 -0600

    Adding Pawn movements

--- A2.py
+++ A2.py
@@ -3,14 +3,31 @@
 # Chad A. Woitas
 
 
-class board:
+class Board:
 
     def __init__(self):
         self.x = 5
         self.y = 5
 
+        self.Q = queen(1,3)
+
+        self.D1 = dragon(2,2)
+        self.D2 = dragon(2,3)
+        self.D3 = dragon(2,4)
+
+        self.P1 = pawn(5, 1)
+        self.P2 = pawn(5, 2)
+        self.P3 = pawn(5, 3)
+        self.P4 = pawn(5, 4)
+        self.P5 = pawn(5, 5)
+
+    # def display(self):
+
 
 class queen:
+    def __init__(self,x,y):
+        self.x
+        self.y
 
     @staticmethod
     def display():
@@ -18,6 +35,9 @@ class queen:
 
 
 class dragon:
+    def __init__(self,x,y):
+        self.x
+        self.y
 
     @staticmethod
     def display():
@@ -25,7 +45,31 @@ class dragon:
 
 
 class pawn:
+    def __init__(self,x,y):
+        self.x
+        self.y
+    def forward(self):
+        if self.x!=5:
+            self.x-=1
+
+    def backward(self):
+        if self.x!=1:
+            self.x+=1
+
+    def left(self):
+        if self.y!=1:
+            self.y-=1
 
     @staticmethod
     def display():
         return 'p'
+
+# class search:
+#
+# class searchNodes:
+#
+# class problemState:
+#
+# class problem:
+#     def __init__(self):
+#         Board board = Board.__init__()
